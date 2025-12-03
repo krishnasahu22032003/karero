29f5942df2545df80cdd393a92dd2bf974bf4ee0
krishna sahu <krishna.sahu.work@gmail.com>
2025-12-04 00:51:57 +0530

    Checkuser logic done

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -1,24 +1,39 @@
 import { currentUser } from "@clerk/nextjs/server"
 import { pg } from "./prisma"
 
-export const CheckUser = async ()=>{
+export const CheckUser = async () => {
 
-const user = await currentUser()
+    const user = await currentUser()
 
-if(!user){
-    return null 
-}
+    if (!user) {
+        return null
+    }
 
-try{
+    try {
 
-    await pg.user.findUnique({
-        where:{
-            clerkUserId:user.id
+        const loggedInUser = await pg.user.findUnique({
+            where: {
+                clerkUserId: user.id
+            }
+        })
+
+        if (loggedInUser) {
+            return loggedInUser;
         }
-    })
 
-}catch(e){
+        const name = `${user.firstName} ${user.lastName}`;
 
-}
+        const newsuer = await pg.user.create({
+            data: {
+                clerkUserId: user.id,
+                name: name,
+                imageUrl: user.imageUrl,
+                email: user.emailAddresses[0].emailAddress
+            }
+        })
+        return newsuer;
+    } catch (e) {
+        console.log((e as Error).message)
+    }
 
 }
