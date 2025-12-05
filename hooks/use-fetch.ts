278a969a600d49a4ae6e61c4b0ba41d4ef2b31d5
krishna sahu <krishna.sahu.work@gmail.com>
2025-12-05 22:44:14 +0530

    Use fetch hook created

---
+++ hooks/use-fetch.ts
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { toast } from "sonner";
+
+function useFetch<TArgs extends any[], TResult>(
+  cb: (...args: TArgs) => Promise<TResult>
+) {
+  const [data, setData] = useState<TResult | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<unknown>(null);
+
+  const fn = async (...args: TArgs): Promise<void> => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await cb(...args);
+      setData(response);
+    } catch (err: unknown) {
+      setError(err);
+
+      const message =
+        err instanceof Error ? err.message : "Something went wrong.";
+
+      toast.error(message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, fn, setData };
+}
+
+export default useFetch;
