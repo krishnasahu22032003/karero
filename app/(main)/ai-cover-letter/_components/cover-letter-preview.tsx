2e2d457f44194030de1e9784d0b72cff84e0f999
krishna sahu <krishna.sahu.work@gmail.com>
2025-12-27 16:05:03 +0530

    cover-letter-preview

---
+++ app/(main)/ai-cover-letter/_components/cover-letter-preview.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import MDEditor from "@uiw/react-md-editor";
+
+const CoverLetterPreview = ({
+  content,
+}: {
+  content: string;
+}) => {
+  return (
+    <div className="py-4">
+      <MDEditor value={content} preview="preview" height={700} />
+    </div>
+  );
+};
+
+export default CoverLetterPreview;
