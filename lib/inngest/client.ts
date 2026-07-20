697baf8c0c668fa46ce3d56d60bc141b8e59252f
krishna sahu <krishna.sahu.work@gmail.com>
2026-07-20 06:09:56 +0530

    ingest client code update

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,10 +1,6 @@
 import { Inngest } from "inngest";
 
-
-export const inngest = new Inngest({  id: "Karero AI",
+export const inngest = new Inngest({
+  id: "Karero AI",
   name: "Karero AI",
-  credentials: {
-    openai: {
-      apiKey: process.env.OPENAI_API_KEY,
-    },
-  },});
+});
